1750ecf49b1dd30fa57e8569538b154e0466d0cd
Sathvik <sathvik.raki@gmail.com>
2023-03-23 13:22:47 -0700

    Added video processing to UI + added necessary .py files for UI

--- UI/main.py
+++ UI/main.py
@@ -3,7 +3,7 @@ import sys
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
-import numpy
+import numpy as np
 import cv2
 from CapstonePY import Ui_MainWindow
 
@@ -74,6 +74,8 @@ class MainWindow:
 
 # Define webcam class
 class WebCam(QThread):
+
+    
     ImageUpdate = pyqtSignal(QImage)
 
     def run(self):
@@ -81,11 +83,41 @@ class WebCam(QThread):
 
         Capture = cv2.VideoCapture(0)
         while self.ThreadActive:
-            ret, frame = Capture.read()
+            a = 0
+            b = 255
+            ret, img = Capture.read()
             if ret:
-                Image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-                Flippedimage = cv2.flip(Image, 1)
-                Qtformat = QImage(Flippedimage.data, Flippedimage.shape[1], Flippedimage.shape[0], QImage.Format_RGB888)
+                kernel = np.ones((5,5), np.uint8)
+
+                img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+                
+                img = cv2.normalize(img,img, a, b, cv2.NORM_MINMAX)
+                
+                # create a CLAHE object (Arguments are optional).
+                clahe = cv2.createCLAHE(clipLimit=1.5, tileGridSize=(7,7))
+                cl1 = clahe.apply(img)
+
+                #apply adaptive threshold
+                cl2 = cv2.medianBlur(cl1, 5)
+                th1 = cv2.adaptiveThreshold(cl2,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY_INV,25,2.55)
+
+                #apply otsu threshold
+                blur = cv2.GaussianBlur(cl1,(5,5),0)
+                ret3,th3 = cv2.threshold(blur,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+
+                #removing noise 
+                th2 = th1 & th3
+
+                #morphological transform
+                opening = cv2.morphologyEx(th2, cv2.MORPH_OPEN, kernel)
+
+                #finding outlines via contouring process
+                contours, hierarchy = cv2.findContours(opening,cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
+                img1 = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR) 
+                dst = cv2.drawContours(img1, contours, -1, (0,255,0), -1)
+                
+                
+                Qtformat = QImage(dst.data, dst.shape[1], dst.shape[0], QImage.Format_RGB888)
                 self.ImageUpdate.emit(Qtformat)
 
 if __name__ == '__main__':
